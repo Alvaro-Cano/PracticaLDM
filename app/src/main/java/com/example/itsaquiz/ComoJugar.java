98da9e9d5250d458b46ada59e7d4d03ca07b2786
Yeray <y.cornejo.2018@alumnos.urjc.es>
2021-10-20 15:43:38 +0200

    Update menu principal

---
+++ app/src/main/java/com/example/itsaquiz/ComoJugar.java
@@ -0,0 +1,14 @@
+package com.example.itsaquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComoJugar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_como_jugar);
+    }
+}
