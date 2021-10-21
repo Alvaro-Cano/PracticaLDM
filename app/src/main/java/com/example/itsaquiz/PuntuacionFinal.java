e2ee8cbbcec5769fc0a1bd5f84eeaac0bab3532a
Alvaro <alvarocg26100@gmail.com>
2021-10-21 17:32:51 +0200

    Pantalla Como jugar

--- app/src/main/java/com/example/itsaquiz/PuntuacionFinal.java
+++ app/src/main/java/com/example/itsaquiz/PuntuacionFinal.java
@@ -6,9 +6,19 @@ import android.os.Bundle;
 
 public class PuntuacionFinal extends AppCompatActivity {
 
+    private int puntuacion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puntuacion_final);
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
     }
 }
