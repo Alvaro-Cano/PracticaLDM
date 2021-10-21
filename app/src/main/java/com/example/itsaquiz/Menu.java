f2da3c2d1c230537ebc1a05fc6b0245bfd86ca07
Yeray <y.cornejo.2018@alumnos.urjc.es>
2021-10-21 10:49:58 +0200

    Update salir menu

--- app/src/main/java/com/example/itsaquiz/Menu.java
+++ app/src/main/java/com/example/itsaquiz/Menu.java
@@ -1,9 +1,12 @@
 package com.example.itsaquiz;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 
 public class Menu extends AppCompatActivity {
@@ -22,5 +25,31 @@ public class Menu extends AppCompatActivity {
     public void ComoJugar (View v){
         Intent como = new Intent(this,ComoJugar.class);
         startActivity(como);
+    }
+
+    public void Salir (View v){
+        finish();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == event.KEYCODE_BACK){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("¿Desea salir del juego?")
+                    .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+            builder.show();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
