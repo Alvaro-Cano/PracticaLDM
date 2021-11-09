31c0ad2de842364d373f456ed913a9a4b45e631d
Alvaro <alvarocg26100@gmail.com>
2021-11-09 19:49:42 +0100

    how to use with 2 lotties

--- app/src/main/java/com/example/practica2/Builds.java
+++ app/src/main/java/com/example/practica2/Builds.java
@@ -1,5 +1,6 @@
 package com.example.practica2;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,11 @@ public class Builds extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_builds);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.mipmap.ic_launcher_2);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        
     }
 }
--- app/src/main/java/com/example/practica2/HowToUse.java
+++ app/src/main/java/com/example/practica2/HowToUse.java
@@ -1,8 +1,12 @@
 package com.example.practica2;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 public class HowToUse extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class HowToUse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_use);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.mipmap.ic_launcher_2);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        TextView tv1 = (TextView)findViewById(R.id.textView);
+        TextView tv2 = (TextView)findViewById(R.id.textView2);
+        TextView tv3 = (TextView)findViewById(R.id.textView3);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // acciones que se ejecutan tras los milisegundos
+                tv1.setVisibility(View.VISIBLE);
+                tv2.setVisibility(View.VISIBLE);
+                tv3.setVisibility(View.VISIBLE);
+            }
+        }, 4000);
     }
 }
